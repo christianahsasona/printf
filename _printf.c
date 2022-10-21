957bd8286d7e10aaa2b2dc7015ef16f1ab51c5d8
christianahsasona <sasonachristianah@gmail.com>
2022-10-21 10:01:24 +0000

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,51 +1,78 @@
 #include "main.h"
-#include <limits.h>
-#include <stdio.h>
-
 /**
- * _printf - produces output according to a format
- * @format: str containing the attributes and the specifiers
- * Description: this function will notify  the get_print() funct that
- * will make out which printing function to call
- * based on the specifiers containng into fmt
- * Return: length f the formatted output string
+ * _printf - printf function.
+ * @format: variable
  *
+ * Return: nbytes printed.
  */
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, flags_t *);
-const char *p;
-va_list arguments;
-flags_t flags = {0, 0, 0};
-register int count = 0;
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
-}
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
-}
-else
-count += _putchar(*p);
-}
-_putchar(-1);
-va_end(arguments);
-return (count);
+	va_list list;
+	unsigned int i = 0, characters_number = 0;
+
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				characters_number++;
+				i++;
+			}
+			else if (cmp_func(format[i + 1]) != NULL)
+			{
+				characters_number += (cmp_func(format[i + 1]))(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				characters_number++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			characters_number++;
+		}
+	}
+	va_end(list);
+	return (characters_number);
 }
 
+/**
+ * cmp_func - Entry point
+ * @a: character.
+ *
+ * Return: 0.
+ */
+int (*cmp_func(const char a))(va_list)
+{
+	print_f printf[] = {
+		{'c', printc},
+		{'s', print_string},
+		{'d', print_n},
+		{'i', print_n},
+		{'\0', NULL}
+	};
 
+	int k;
+
+	for (k = 0; printf[k].p != '\0'; k++)
+	{
+		if (printf[k].p == a)
+		{
+			return (printf[k].func);
+		}
+	}
+
+	return (0);
+}
